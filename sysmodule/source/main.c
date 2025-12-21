5511da3b0df5c765389344787561c7d12c0533bf
nangongjing1 <480687007@qq.com>
2025-12-21 22:16:03 +0800

    64KB 的内部堆就够用, 现在运行时的的内存占用 0.22MB

--- sysmodule/source/main.c
+++ sysmodule/source/main.c
@@ -1,7 +1,7 @@
 #include "fancontrol.h"
 
-// Size of the inner heap (adjust as necessary). 192KB now.
-#define INNER_HEAP_SIZE 0x30000
+// Size of the inner heap (adjust as necessary). 64KB now.
+#define INNER_HEAP_SIZE 0x10000
 
 #ifdef __cplusplus
 extern "C" {
